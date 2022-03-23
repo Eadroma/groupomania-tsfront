8ab6231c9a7ce8e2977e88577bd2bfd47203e3fb
Eadroma <boukottaya.jr@gmail.com>
2022-03-23 11:58:10 +0100

    [ADD] some basic Page
    - Profile page
    - Home page

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,23 +1,52 @@
-import { useEffect, useState } from "react";
-import Sidebar from "../../components/Sidebar/Sidebar"
-
-export default function Profile() {
-    const [data, setData] = useState({});
-    const [isLoading, setLoading] = useState(false);
-    const [posts, setPosts] = useState([]);
-    const [loStorage, setLoStorage] = useState(JSON.parse(localStorage.getItem('user') as string));
-    
-    useEffect(() => {
-        setLoading(true);
-        return () => {
-            setLoading(false);
-        }
-    })
-   
+import React from 'react';
+import { useEffect, useState } from 'react';
+import Sidebar from '../../components/Sidebar/Sidebar';
+import Posts from '../../components/Post/post';
+import './style.css';
+import getUserbyId from '../../hooks/getUser';
+import { getItemLocalStorage } from '../../hooks/getLocalStorage';
+import { Avatar, Card, CardContent, CardHeader, CardMedia, IconButton, Typography } from '@mui/material';
+import addPost from '../../components/addPost/addPost';
+const ProfileHeader = () => {
+    const queryString = window.location.search;
+    const objectId = new URLSearchParams(queryString).get('id');
+    const loStorage = getItemLocalStorage();
+    let userService = getUserbyId(loStorage.id);
+    if (objectId) userService = getUserbyId(parseInt(objectId));
     return (
-        <>
-        <Sidebar />
-
-        </>
-    )
+        <div>
+            {userService.status === 'loading' && <div>Loading...</div>}
+            {userService.status === 'loaded' && userService.status === 'loaded' && (
+                <Card className="layoutHeader">
+                    <CardMedia component="img" height="275" image={userService.payload.coverUrl} alt="cover img" />
+                    <CardHeader
+                        avatar={
+                            <Avatar
+                                aria-label="recipe"
+                                src={userService.payload.imgUrl}
+                                sx={{ width: 92, height: 92 }}
+                            />
+                        }
+                        action={<IconButton aria-label="settings"></IconButton>}
+                        title={`${userService.payload.name} - ${userService.payload.email}`}
+                        subheader={userService.payload.description}
+                    />
+                </Card>
+            )}
+            {userService.status === 'error' && <div>Error, the backend moved to the dark side.</div>}
+        </div>
+    );
+};
+export default function Profile() {
+    return (
+        <div className="container">
+            <Sidebar />
+            <div className="layout">
+                <ProfileHeader />
+                {addPost()}
+                <h2 id="posts">Posts:</h2>
+                <Posts />
+            </div>
+        </div>
+    );
 }
