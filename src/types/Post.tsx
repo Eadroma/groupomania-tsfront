75772261ad479f0f6f27d92bfa017b96946d9b75
Eadroma <boukottaya.jr@gmail.com>
2022-03-23 11:55:04 +0100

    [ADD] definition of my data type

---
+++ src/types/Post.tsx
@@ -0,0 +1,8 @@
+export type Post = {
+    date: string;
+    id: number;
+    imgUrl: string | null;
+    likes: number;
+    textContent: string;
+    userId: number;
+};
---
+++ src/types/User.tsx
@@ -0,0 +1,10 @@
+export type User = {
+    coverUrl: string;
+    description: string;
+    email: string;
+    id: number;
+    imgUrl: string;
+    name: string;
+    password: string;
+    token: string | null;
+};
